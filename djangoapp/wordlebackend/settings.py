90e3b65b967320a990f469e7eec5554f60db695f
Gregorius Avip <71865513+gregoriusavip@users.noreply.github.com>
2026-04-14 20:47:32 -0700

    Add .env

--- djangoapp/wordlebackend/settings.py
+++ djangoapp/wordlebackend/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv 
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s2=yt4$ux4ygh6$$88t2$rn95yz(ki)3cm#)hi@6fwe6im_2*l'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
