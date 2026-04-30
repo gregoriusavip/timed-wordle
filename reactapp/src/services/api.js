370248c1b08c1bad3ffaa025dd555513aa7e254c
Gregorius Avip <71865513+gregoriusavip@users.noreply.github.com>
2026-04-30 13:11:56 -0700

    Clean up so the api handle singular responsibility

--- reactapp/src/services/api.js
+++ reactapp/src/services/api.js
@@ -1,37 +1,31 @@
+/**
+ * A schema for user's guess
+ * @typedef {Object} Guess
+ * @property {string} curGuess - User's guess, must be length of 5 and only alphabets
+ * @property {string} prevGuess - User previous round's guess,
+ * @property {boolean} mode - Hard mode settings
+ * @property {string} curDate - Current date in format: YYYY-MM-DD
+ */
+
 const BASE_URL = "";
 
 /**
- * Get client's date based of their timezone and format it
- * @returns {String} Formatted date YYYY-MM-DD
+ * @param {...Guess} data - {@link Guess}'s schema
+ * @throws - Throws an error if response is not OK (status code != 200-299)
+ * @returns {Promise<Object>} - Returns the JSON with hints based of the guess
  */
-function getCurrentDate() {
-  let currentDate = new Date();
-  const offset = currentDate.getTimezoneOffset();
-  currentDate = new Date(currentDate.getTime() - offset * 60 * 1000);
-
-  return currentDate.toISOString().split("T")[0];
-}
-
-/**
- * @param {String} curGuess 5 length word of the current guess
- * @param {Boolean} [hardMode=false]
- * @param {String} [prevGuess=""] 5 length word of the previous guess, could be an empty string
- * @throws Throws an error if response is not OK (status code != 200-299)
- * @returns {Promise<Object>} Returns the JSON with hints based of the guess
- */
-async function getGradedGuess(curGuess, hardMode = false, prevGuess = "") {
+async function getGradedGuess(data) {
   const url = `${BASE_URL}/api/guess`;
-  const currentDate = getCurrentDate();
   const response = await fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      guess: curGuess,
-      date: currentDate,
-      hard_mode: hardMode,
-      prev_guess: prevGuess,
+      guess: data.curGuess,
+      date: data.curDate,
+      hard_mode: data.mode,
+      prev_guess: data.prevGuess,
     }),
   });
   if (!response.ok) {
