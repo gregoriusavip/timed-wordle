70efc569dd42126dff5ba6f34708cdd868bf25b6
Gregorius Avip <71865513+gregoriusavip@users.noreply.github.com>
2026-04-30 23:04:28 -0700

    Refactor guess submission logic to use mutation and handle validation errors

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -1,66 +1,70 @@
 import Grid from "./game/Grid";
 import "./App.css";
 import { useState, useEffect } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { getGradedGuess } from "./services/api";
+import { getCurrentDate } from "./services/utils";
+import { GuessSchema } from "./services/schema";
+import * as z from "zod";
 
 function App() {
   const [guess, setGuess] = useState("");
   const [guesses, setGuesses] = useState([]);
   const [gameStatus, setGameStatus] = useState("playing");
-  const [invalidReason, setInvalidReason] = useState();
   const [hardMode, setHardMode] = useState(false);
 
-  function validateHardMode() {
-    // Handle hard mode validation
-  }
+  const mutation = useMutation({
+    mutationFn: () => {
+      const data = {
+        curGuess: guess,
+        prevGuess: guesses.at(-1) ?? "",
+        mode: hardMode,
+        curDate: getCurrentDate(),
+      };
+      try {
+        const validGuess = GuessSchema.parse(data);
+        return getGradedGuess(validGuess);
+      } catch (e) {
+        if (e instanceof z.ZodError) {
+          // Schema validation error
+          throw new Error(e.issues[0].message);
+        }
 
-  function checkValidGuesses() {
-    // Handle checking the list of valid guess
-    return true; // placeholder
-  }
-
-  function validateWord() {
-    if (guess.length < 5) {
-      setInvalidReason("Not enough letters");
-      return false;
-    } else if (hardMode && validateHardMode()) {
-      setInvalidReason("Not using previous hints");
-      return false;
-    } else if (!checkValidGuesses()) {
-      setInvalidReason("Not a valid word");
-      return false;
-    } else {
-      return true;
-    }
-  }
-
-  function submitGuess() {
-    if (validateWord()) {
+        // API error
+        throw new Error(e.message);
+      }
+    },
+    onSuccess: (data) => {
       setGuesses((prevGuesses) => [...prevGuesses, guess]);
       setGuess("");
-    } else {
-      // Do some animation and show some pop up that the guess is invalid for x reason
-    }
-  }
-
-  function handleKeyDown(e) {
-    if (e.key === "Enter") {
-      submitGuess();
-    } else if (e.key === "Backspace" || e.key === "Delete") {
-      setGuess((prev) => prev.slice(0, -1));
-    } else if (/^[a-zA-Z]$/.test(e.key) && guess.length < 5) {
-      setGuess((prev) => prev + e.key.toUpperCase());
-    }
-  }
+    },
+  });
 
   useEffect(() => {
     if (gameStatus !== "playing") return;
+
+    async function handleKeyDown(e) {
+      if (e.key === "Enter") {
+        mutation.mutate();
+      } else if (e.key === "Backspace" || e.key === "Delete") {
+        setGuess((prev) => prev.slice(0, -1));
+      } else if (/^[a-zA-Z]$/.test(e.key) && guess.length < 5) {
+        setGuess((prev) => prev + e.key.toUpperCase());
+      }
+    }
+
     window.addEventListener("keydown", handleKeyDown);
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
-  }, [guess, gameStatus]);
+  }, [gameStatus, guess, mutation]);
 
-  return <Grid guesses={guesses} currentGuess={guess}></Grid>;
+  return (
+    <>
+      {mutation.isError ? <div>{mutation.error.message}</div> : null}
+      <Grid guesses={guesses} currentGuess={guess}></Grid>
+    </>
+  );
 }
 
 export default App;
