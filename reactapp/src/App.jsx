640e76fab807a29c03bd516a7396e7154ad38b2b
Gregorius Avip <71865513+gregoriusavip@users.noreply.github.com>
2026-05-18 02:07:12 -0700

    Add logic to remove timer from DOM

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -1,4 +1,5 @@
 import Grid from "./components/game/Grid";
+import { Keyboard } from "./components/ui/Keyboard";
 import "./App.css";
 import Countdown from "react-countdown";
 import { useGame } from "./hooks/useGame";
@@ -12,15 +13,20 @@ function App() {
     targetTime,
     multiplier,
     error,
+    keyboardStatus,
+    showCountdown,
     handleInput,
     handleTimeout,
   } = useGame();
+
   // Random component
   const Completionist = () => <span>Random guess NOW!</span>;
 
   // Renderer callback with condition
   const renderer = ({ seconds, completed }) => {
-    if (gameStatus !== "playing") return null; // Remove timer
+    if (gameStatus !== "playing") {
+      return null;
+    }
     if (completed) {
       return <Completionist />;
     } else {
@@ -36,14 +42,17 @@ function App() {
         : gameStatus === "gameOver"
           ? `You Lost. The word is: ${solution}`
           : null}
-      <Countdown
-        date={targetTime}
-        key={multiplier}
-        renderer={renderer}
-        onComplete={handleTimeout}
-      />
+      {showCountdown ? (
+        <Countdown
+          date={targetTime}
+          key={multiplier}
+          renderer={renderer}
+          onComplete={handleTimeout}
+        />
+      ) : null}
       {error}
       <Grid attempts={attempts} currentGuess={guess} />
+      <Keyboard keyboardStatus={keyboardStatus} handleInput={handleInput} />
     </>
   );
 }
