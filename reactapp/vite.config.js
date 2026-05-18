504344333107149bf1c99503c22eb839f3f3c58c
Gregorius Avip <71865513+gregoriusavip@users.noreply.github.com>
2026-05-18 02:06:49 -0700

    Add svgr plugin

--- reactapp/vite.config.js
+++ reactapp/vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), svgr()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:8000', // Backend server
-        changeOrigin: true // Ensure the request appears to come from the frontend server
+      "/api": {
+        target: "http://localhost:8000", // Backend server
+        changeOrigin: true, // Ensure the request appears to come from the frontend server
       },
-    }
-  }
-})
+    },
+  },
+});
