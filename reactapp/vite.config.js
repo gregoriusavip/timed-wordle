c3fe8be4098d53a4330d8def8de96c565ac7b190
Gregorius Avip <71865513+gregoriusavip@users.noreply.github.com>
2026-06-15 20:45:15 -0700

    Add docker for frontend

--- reactapp/vite.config.js
+++ reactapp/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000", // Backend server
+        target: "http://api-django:8000", // Backend server
         changeOrigin: true, // Ensure the request appears to come from the frontend server
       },
     },
